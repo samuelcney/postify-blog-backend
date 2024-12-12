733b05b4a1aaa9d36194808f1156691077c3971e
Samuel Costa Ney <SCOSTA@VIPTECHTI>
2024-12-12 09:45:03 -0300

    fix

--- src/main/java/com/spring/app/postify/config/WebConfig.java
+++ src/main/java/com/spring/app/postify/config/WebConfig.java
@@ -9,6 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+        registry.addMapping("/**").allowedOrigins("http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
