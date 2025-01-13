e53e64c94bfec91fa15b427d29ddd84aa2dc9d6f
sapato social <samuelcney2@gmail.com>
2025-01-13 17:35:00 -0300

    fix(auth): fixing login endpoint returns and adding jwt token

--- src/main/java/com/spring/app/postify/service/LoginService.java
+++ src/main/java/com/spring/app/postify/service/LoginService.java
@@ -1,12 +1,17 @@
 package com.spring.app.postify.service;
 
+import com.spring.app.postify.dto.LoginResponseDTO;
 import com.spring.app.postify.dto.UserRequestDTO;
 import com.spring.app.postify.model.User;
 import com.spring.app.postify.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -18,7 +23,10 @@ public class LoginService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String login(UserRequestDTO userDto){
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public LoginResponseDTO login(UserRequestDTO userDto){
 
         Optional<User> user = this.userRepository
                 .findByEmail(userDto.email());
@@ -33,6 +41,13 @@ public class LoginService {
             throw new IllegalArgumentException("Credenciais inválidas");
         }
 
-        return "Login bem-sucedido! Seja bem vindo " + user.get().getFirstName();
+        String token = Jwts.builder()
+                .setSubject(user.get().getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+        return new LoginResponseDTO(token, user.get());
     }
 }
